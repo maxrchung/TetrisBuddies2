e8425fa98acfc99682c41869f3d8de0de50a4e11
Winston Liang <winstoyl@uci.edu>
2014-12-29 10:13:14 -0800

    delay when you hit something

--- Game/gravity2.py
+++ Game/gravity2.py
@@ -5,14 +5,23 @@ class gravity:
         self._time = pygame.time.get_ticks()
         self._dropTime = acc
         self._increment = inc
+        self._landing = False
         
     def fall(self,block,cells):
-        if pygame.time.get_ticks() - self._time > self._dropTime:
+        if (pygame.time.get_ticks() - self._time > self._dropTime
+            and self._landing == False):
+            self._time = pygame.time.get_ticks()
             if(cells.checkCol(block)==False):
                 block.y += 1
+                print('jalapeno?')
             else:
-                block=cells.place(block)
-            self._time = pygame.time.get_ticks()
+                self._landing = True
             if self._dropTime > 300 :
                 self._dropTime -= self._increment
-        return block   
+        elif (self._landing == True and pygame.time.get_ticks() -
+              self._time > 300):
+            print('???')
+            self._landing = False
+            block = cells.place(block)
+            self._time = pygame.time.get_ticks()
+        return block
