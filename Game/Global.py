1d1b142dd5306774a626adfedd16a70038856963
maxrc <maxrc@uci.edu>
2014-12-30 13:16:58 -0800

    Additional changes for singleplayer

--- Game/Global.py
+++ Game/Global.py
@@ -1,7 +1,2 @@
 # Global references that we'll need
-player = None
-opponent = None
-NetworkManager = None
 SoundManager = None
-Game = None
-GameBoard = None
--- Game/TetrisBuddies.py
+++
@@ -1,6 +0,0 @@
-import Global
-from Game import Game
-
-Global.Game = Game()
-Global.Game.initialize()
-Global.Game.run()
